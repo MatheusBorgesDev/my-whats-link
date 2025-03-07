5d1cce82a3ac0199781f22b4a9669ab6f863b2a1
Matheus Borges <matheusb.dev@gmail.com>
2025-03-07 15:06:32 -0300

    feat: :sparkles: integrate GetLinkForm component into App for user input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GetLinkForm } from "./components/ui/get-link-form";
+
 function App() {
 	return (
-		<main>
+		<main className="h-screen bg-[url(./assets/background.jpg)] font-rubik">
 			<h1 className="">My Whats Link</h1>
+			<GetLinkForm />
 		</main>
 	);
 }
